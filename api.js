800a32e9f83afa77caf49f3c7142c951667a2dcc
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2024-01-31 22:16:38 +0100

    feat: add firebase and firestore data to the file

--- api.js
+++ api.js
@@ -1,3 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYmiCdHRhGQrjPyFqwJXtk1YnSQe2SfOY",
+  authDomain: "reactrouterdom-datalayerapi.firebaseapp.com",
+  projectId: "reactrouterdom-datalayerapi",
+  storageBucket: "reactrouterdom-datalayerapi.appspot.com",
+  messagingSenderId: "793555133653",
+  appId: "1:793555133653:web:05f58a205ed68487e6ee5c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+
 export async function getVans(id) {
     const url = id ? `/api/vans/${id}` : "/api/vans"
     const res = await fetch(url)
